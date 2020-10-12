eac9c5b69162e9460a4c558c6495e426c34a9a66
Vadym Vorobel <vadym.vorobel@gmail.com>
2020-10-12 21:51:02 +0300

    Add HTTP methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Get,
+  Body,
+  Post,
+  Param,
+  Patch,
+  Delete,
+  Controller,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees'
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery
+
+    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+  
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This action deletes #${id} coffee`;
   }
 }
