c3ccaf7a72736a001363ac08e1044411d31374dd
Vadym Vorobel <vadym.vorobel@gmail.com>
2020-10-17 11:24:57 +0300

    Add coffee collection

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CoffeesController } from './coffees.controller'
-import { CoffeesService } from './coffees.service'
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
   controllers: [CoffeesController],
-  providers: [CoffeesService]
+  providers: [CoffeesService],
 })
 export class CoffeesModule {}
