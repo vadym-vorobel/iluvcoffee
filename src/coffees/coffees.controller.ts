f9457742d7b3477f8ec3793d5d251df5fc68522e
Vadym Vorobel <vadym.vorobel@gmail.com>
2020-10-21 22:35:21 +0300

    Add pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,6 +8,7 @@ import {
   Controller,
   Query,
 } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -18,10 +19,8 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) { }
 
   @Get()
-  findAll(@Query() paginationQuery) {
-    // const { limit, offset } = paginationQuery
-
-    return this.coffeesService.findAll()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeesService.findAll(paginationQuery)
   }
 
   @Get(':id')
